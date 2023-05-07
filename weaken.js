8dddbccba6a439dbfd6a9c3450bbc471a944139e
Tamagosci <rodisimo2@gmail.com>
2023-05-07 21:15:49 +0200

    Added port atExit option

--- weaken.js
+++ weaken.js
@@ -1,5 +1,6 @@
 /** @param {NS} ns */
 export async function main(ns) {
-	let [target, delay = 0, affectStocks = false] = ns.args;
-	await ns.weaken(target, {additionalMsec: delay, stock: affectStocks});
+	let [target, delay = 0, affectStocks = false, port = 0, data] = ns.args;
+	await ns.weaken(target, { additionalMsec: delay, stock: affectStocks });
+	if (port !== 0) ns.atExit(() => ns.tryWritePort(port, data ?? ns.pid));
 }
