74917e9d0cfb0bcb9830a4a47e3067a06626ac71
Tamagosci <rodisimo2@gmail.com>
2023-05-18 14:49:40 +0200

    Added maxServer option

--- hashnet.js
+++ hashnet.js
@@ -15,12 +15,12 @@ const HASH_STORE = {
 
 /** @param {NS} ns */
 export async function main(ns) {
-
-	await hashnet(ns);
+	let [target] = ns.args;
+	if (target !== undefined && ns.serverExists(target)) spendHashesToMaxServer(ns, target);
+	else await hashnet(ns);
 }
 
 const DEBUG_MODE = false;
-
 
 const MAX_NODES = 20;
 const MAX_LEVEL = 300;
@@ -28,8 +28,8 @@ const MAX_RAM = 8192;
 const MAX_CORES = 128;
 const MAX_CACHE = 15;
 
-const ALLOWED_FUNDS = 0.05;
-const SLEEP = 12e3;
+const ALLOWED_FUNDS = 0.02;
+const SLEEP = 6e3;
 
 let BITNODE_MULT = 1;
 
@@ -41,6 +41,7 @@ async function hashnet(ns) {
 	ns.disableLog('ALL');
 	ns.tail();
 	await ns.sleep(0);
+	ns.moveTail(1081, 28);
 
 	//Variables setup
 	BITNODE_MULT = ns.getBitNodeMultipliers().HacknetNodeMoney;
@@ -60,11 +61,11 @@ async function hashnet(ns) {
 		//Attempt to buy more nodes
 		nodesOwned = ns.hacknet.numNodes();
 		while (nodesOwned < MAX_NODES && nextNodeCost <= funds) {
+			if (ns.hacknet.purchaseNode() === -1) break;
 			funds -= nextNodeCost;
-			ns.hacknet.purchaseNode();
 			nextNodeCost = ns.hacknet.getPurchaseNodeCost();
-			if (DEBUG_MODE) ns.print('Bought hashnet node ' + nodesOwned);
-			else report(ns);
+			ns.print('INFO Bought hashnet node ' + nodesOwned);
+			report(ns);
 			nodesOwned++;
 		}
 
@@ -74,15 +75,15 @@ async function hashnet(ns) {
 			allNodeUpgrades = allNodeUpgrades.concat(getUpgradeDetails(ns, i));
 		//Priority for highest relative gain for hash upgrades and cost for cache
 		allNodeUpgrades.sort((a, b) => a.cost - b.cost).sort((a, b) => b.relativeGain - a.relativeGain);
-		
+
 		//Upgrade hash production in order of priority
 		for (const upgrade of allNodeUpgrades) {
 			//ns.print(`DEBUG Looking at node ${upgrade.index} type ${upgrade.type} cost ${upgrade.cost} funds ${funds}`)
 			if (upgrade.cost <= funds) {
 				eval('ns.hacknet.upgrade' + upgrade.type + '(' + upgrade.index + ', 1);');
 				funds -= upgrade.cost;
-				if (DEBUG_MODE) ns.print(`Upgraded node ${upgrade.index} ${upgrade.stat} to ${ns.hacknet.getNodeStats(upgrade.index)[upgrade.stat]}`);
-				else report(ns);
+				ns.print(`INFO Upgraded node ${upgrade.index} ${upgrade.stat} to ${ns.hacknet.getNodeStats(upgrade.index)[upgrade.stat]}`);
+				report(ns);
 			}
 		}
 
@@ -105,7 +106,7 @@ async function hashnet(ns) {
  */
 function getUpgradeDetails(ns, nodeIndex) {
 	const node = ns.hacknet.getNodeStats(nodeIndex);
-	const currentGain =  ns.formulas.hacknetServers.hashGainRate(node.level, 0, node.ram, node.cores, BITNODE_MULT);
+	const currentGain = ns.formulas.hacknetServers.hashGainRate(node.level, 0, node.ram, node.cores, BITNODE_MULT);
 	//Level
 	const level = {
 		index: nodeIndex,
@@ -156,9 +157,33 @@ function areAllNodesMaxed(ns) {
 	let node;
 	for (let i = 0; i < nodesOwned; i++) {
 		node = ns.hacknet.getNodeStats(i);
-		if (node.cache < MAX_CACHE || node.cores < MAX_CORES|| node.ram < MAX_RAM || node.level < MAX_LEVEL ) return false;
+		if (node.cache < MAX_CACHE || node.cores < MAX_CORES || node.ram < MAX_RAM || node.level < MAX_LEVEL) return false;
 	}
 	return true;
+}
+
+/**
+ * @param {NS} ns
+ * @param {string} target
+ */
+function spendHashesToMaxServer(ns, target) {
+	let purchasedAnything = false;
+	if (ns.hacknet.hashCost(HASH_STORE.minSecurity, 1) > ns.hacknet.hashCapacity())
+		ns.tprint('WARN Cost to lower security exceeds current hash capacity!');
+	else while (ns.hacknet.numHashes() > ns.hacknet.hashCost(HASH_STORE.minSecurity, 1) && ns.getServerMinSecurityLevel(target) > 1) {
+		ns.hacknet.spendHashes(HASH_STORE.minSecurity, target, 1);
+		ns.tprint(`INFO Lowered ${target}'s minimum security to ${ns.getServerMinSecurityLevel(target).toFixed(3)}`);
+		purchasedAnything = true;
+	}
+	if (ns.hacknet.hashCost(HASH_STORE.maxMoney, 1) > ns.hacknet.hashCapacity())
+		ns.tprint('WARN Cost to raise money cap exceeds current hash capacity!');
+	else if (ns.getServerMinSecurityLevel(target) <= 1) while (ns.hacknet.numHashes() > ns.hacknet.hashCost(HASH_STORE.maxMoney, 1) && ns.getServerMaxMoney(target) < 10e12) {
+		ns.hacknet.spendHashes(HASH_STORE.maxMoney, target, 1);
+		ns.tprint(`INFO Increased ${target}'s maximum money to ${ns.getServerMaxMoney(target).toFixed(3)}`);
+		purchasedAnything = true;
+	}
+	if (purchasedAnything) ns.tprint('WARN Ran out of hashes');
+	else ns.tprint('WARN Not enough hashes to purchase anything');
 }
 
 /** @param {NS} ns */
@@ -189,7 +214,7 @@ function report(ns) {
 		formattedCores = node.cores.toString().padStart(node.cores < 10 ? 3 : 4, ' ').padEnd(5, ' ');
 		formattedCache = node.cache.toString().padStart(node.cache < 10 ? 3 : 4, ' ').padEnd(5, ' ');
 		formattedRam = ns.formatRam(node.ram, 0).padStart(5, ' ');
-		formattedHash = formatMoney(node.production, 3).padStart(6, ' ') + ' h/s';
+		formattedHash = formatMoney(node.production, 2).padStart(6, ' ') + ' h/s';
 		totalHashRate += node.production;
 		ns.print(`║  ${formattedNode}  │ ${formattedLevel} │ ${formattedCores} │ ${formattedCache} │ ${formattedRam} │ ${formattedHash} ║`);
 	}
